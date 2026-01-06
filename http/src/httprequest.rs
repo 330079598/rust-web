cf59d4f31a9771ab9045d254279304f4b5fd3913
stone <330079598@qq.com>
2026-01-06 23:33:26 +0800

    feat: parse HTTP requests

--- http/src/httprequest.rs
+++ http/src/httprequest.rs
@@ -47,6 +47,63 @@ pub struct HttpRequest {
     pub msg_body: String,
 }
 
+impl From<String> for HttpRequest {
+    fn from(req: String) -> Self {
+        let mut parsed_method = Method::Uninitialized;
+        let mut parsed_version = Version::V1_1;
+        let mut parsed_resource = Resource::Path(String::new());
+        let mut parsed_headers = HashMap::new();
+        let mut parsed_msg_body = "";
+
+        for line in req.lines() {
+            if line.contains("HTTP") {
+                let (method, resource, version) = process_req_line(line);
+                parsed_method = method;
+                parsed_version = version;
+                parsed_resource = resource;
+            } else if line.contains(":") {
+                let (key, value) = process_header_line(line);
+                parsed_headers.insert(key, value);
+            } else if line.len() == 0 {
+            } else {
+                parsed_msg_body = line;
+            }
+        }
+        HttpRequest {
+            method: parsed_method,
+            version: parsed_version,
+            resource: parsed_resource,
+            headers: parsed_headers,
+            msg_body: parsed_msg_body.to_string(),
+        }
+    }
+}
+
+fn process_header_line(s: &str) -> (String, String) {
+    let mut header_items = s.split(":");
+    let mut key = String::from("");
+    let mut value = String::from("");
+    if let Some(k) = header_items.next() {
+        key = k.to_string();
+    }
+    if let Some(v) = header_items.next() {
+        value = v.to_string();
+    }
+    (key, value)
+}
+
+fn process_req_line(s: &str) -> (Method, Resource, Version) {
+    let mut words = s.split_whitespace();
+    let method = words.next().unwrap();
+    let resource = words.next().unwrap();
+    let version = words.next().unwrap();
+    (
+        method.into(),
+        Resource::Path(resource.to_string()),
+        version.into(),
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -60,5 +117,22 @@ mod tests {
     fn test_version_into() {
         let v: Version = "HTTP/1.1".into();
         assert_eq!(v, Version::V1_1);
+    }
+
+    #[test]
+    fn test_read_http() {
+        let http_request_str = "GET /index.html HTTP/1.1\r\n\
+                                Host: localhost:8080\r\n\
+                                Content-Type: application/json\r\n\
+                                \r\n\
+                                {\"key\": \"value\"}"
+            .to_string();
+
+        let request: HttpRequest = http_request_str.into();
+
+        // 验证请求行解析
+        assert_eq!(request.method, Method::Get);
+        assert_eq!(request.resource, Resource::Path("/index.html".to_string()));
+        assert_eq!(request.version, Version::V1_1);
     }
 }
