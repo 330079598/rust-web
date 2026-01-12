58f9d664117bf5b41b49239bace034897ad71d4e
stone <330079598@qq.com>
2026-01-12 22:43:51 +0800

    fix: resolve path issues

--- httpserver/src/handler.rs
+++ httpserver/src/handler.rs
@@ -8,7 +8,7 @@ pub trait Handler {
     fn handle(req: &HttpRequest) -> HttpResponse;
 
     fn load_file(file_name: &str) -> Option<String> {
-        let default_path = format!("{}/index.html", env!("CARGO_MANIFEST_DIR"));
+        let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
         let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
         let full_path = format!("{}/{}", public_path, file_name);
         let contents = fs::read_to_string(full_path);
